7af88c10af0a9d92e2e37398ea47d74069c67b4b
fabiconcept <favourajokubi@gmail.com>
2025-08-15 13:57:48 +0100

    Space for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -46,6 +46,7 @@ export default function Home() {
               <AnimatedEntrance {...ANIMATION_PRESETS.TEXT_FADE_RIGHT} delay={400}>
                 <p className="text-sm sm:text-base text-gray-600 leading-relaxed">
                   We are committed to coordinating special assignments and handling strategic government interventions that cut across various sectors in the state. Whether responding to emergencies, overseeing special projects, or facilitating high-impact initiatives, our role is central to the smooth operation of government and the achievement of state-wide development goals. Our ministry acts as a bridge between the Governor&rsquo;s special directives and the implementing agencies, ensuring that every assignment is executed with precision, urgency, and public impact.
+                  
                 </p>
               </AnimatedEntrance>
               <AnimatedEntrance {...ANIMATION_PRESETS.BUTTON_SCALE_UP} delay={600}>
